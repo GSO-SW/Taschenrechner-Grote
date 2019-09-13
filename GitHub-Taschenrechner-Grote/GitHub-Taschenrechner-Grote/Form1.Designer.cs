3a682e0a0f647ad8c71c4807fff88fb51663bec4
TreeBoiGreg <saxo961@gmail.com>
2019-09-13 13:07:40 +0200

    Addition von Ganzzahlen

---
+++ GitHub-Taschenrechner-Grote/GitHub-Taschenrechner-Grote/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GitHub_Taschenrechner_Grote
+{
+    partial class frmTaschenrechner
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCalc = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCalc
+            // 
+            this.txtCalc.Location = new System.Drawing.Point(12, 25);
+            this.txtCalc.Name = "txtCalc";
+            this.txtCalc.Size = new System.Drawing.Size(138, 20);
+            this.txtCalc.TabIndex = 0;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(12, 9);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(138, 13);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Geben sie eine Aufgabe ein";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(156, 25);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(20, 20);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "=";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(182, 26);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(138, 20);
+            this.txtResult.TabIndex = 3;
+            // 
+            // frmTaschenrechner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtCalc);
+            this.Name = "frmTaschenrechner";
+            this.Text = "Taschenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCalc;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ GitHub-Taschenrechner-Grote/GitHub-Taschenrechner-Grote/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitHub_Taschenrechner_Grote
+{
+    public partial class frmTaschenrechner : Form
+    {
+        public frmTaschenrechner()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //Leerstellen entfernen
+
+            string input = txtCalc.Text.Replace(" ","");
+            int result = 0;
+
+            try
+            {
+                //Input aufteilen 
+
+                string[] inputStringArr = input.Split('+');
+
+                //input Konvertieren
+
+                int[] inputIntArr = inputStringArr.Select(x => Convert.ToInt32(x)).ToArray();
+
+                //summieren
+
+                result = inputIntArr.Sum();
+
+                txtResult.Text = result.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Falsche Eingabe!");
+            }
+            
+            
+            
+
+
+        }
+    }
+}
